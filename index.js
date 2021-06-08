d24cc7d5efc1454daeda35d57e1d2654e6ad2e00
Shahanul Haque Shawon <37122193+shawon1fb@users.noreply.github.com>
2021-06-09 01:22:21 +0600

    init 2

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-console.log('kkk');
-const t = 0;
+const http = require('http');
 
-console.log(t);
+const app = {};
+app.config = {
+    port: 3000,
+};
+
+app.createServer = () => {
+    const server = http.createServer(app.handelRequest);
+    server.listen(app.config.port, () => {
+        console.log(`listing 1 to port ${app.config.port}`);
+    });
+};
+
+app.handelRequest = (req, res) => {
+    res.end('hello world 4');
+};
+
+app.createServer();
